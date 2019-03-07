be470f80312f49de469d4f20c43fdfe0b4f7da8a
Jay-Mithani <jayvmithani@gmail.com>
2019-03-07 15:43:03 -0500

    update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.post('/prediction', async(req,res) => {
     console.log(req.body);
 
     let spawn = require("child_process").spawn; 
-    let process = spawn('python' ,["./executeModel.py",
+    let process = spawn('python3' ,["./executeModel.py",
     req.body.Title,req.body.ShortPitch,req.body.Description,req.body.Tags]);
 	console.log('process spawned');
     process.stdout.on('data', data=>{
