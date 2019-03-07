011aea43ee7416f94ec6d77f827ce57b0845ef54
mickey henry parker <henryparker594@gmail.com>
2019-03-07 15:29:23 -0500

    update prediction route

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ app.post('/prediction', async(req,res) => {
     console.log(req.body);
 
     let spawn = require("child_process").spawn; 
-    let process = spawn('python',["./executeModel.py",
+    let process = spawn('python3 ',["./executeModel.py",
     req.body.Title,req.body.ShortPitch,req.body.Description,req.body.Tags]);
 
     process.stdout.on('data', data=>{
-        res.send(data);
+        console.log(data);
     }
     )
 
