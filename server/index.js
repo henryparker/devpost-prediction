4708f83582fefccc42d2d518ab33852fca24f1aa
mickey henry parker <henryparker594@gmail.com>
2019-03-07 17:39:07 -0500

    send static html

--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ app.use(bodyParser.json());
 
 let Port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 app.post('/prediction', async(req,res) => {
 
     console.log(req.body);
