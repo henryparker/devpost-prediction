0e2c7c98368964ff56c199743625e6c498c7364f
Jay-Mithani <jayvmithani@gmail.com>
2019-03-07 16:22:34 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -13,13 +13,13 @@ app.post('/prediction', async(req,res) => {
     console.log(req.body);
 
     let spawn = require("child_process").spawn; 
-    let process = spawn('python3' ,["./executeModel.py",
+    let process = spawn('python' ,["./executeModel.py",
     req.body.Title,req.body.ShortPitch,req.body.Description,req.body.Tags]);
 	console.log('process spawned');
     process.stdout.on('data', data=>{
        res.send({percent:data.toString()});
     })
-
+	res.send(Math.random());
 })
 
 app.get('/',async(req,res)=>{
