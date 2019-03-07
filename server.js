3ffd81188b96c91d6d18ac814862aa1c5bbf57f0
Jay-Mithani <jayvmithani@gmail.com>
2019-03-07 18:33:33 -0500

    complete rewrite

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+let port = 5000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.post('/prediction', async(req,res) => {
+
+	console.log(req.body);
+
+	let spawn = require("child_process").spawn; 
+	let process = spawn('python' ,["./model/executeModel.py",
+	req.body.Title,req.body.ShortPitch,req.body.Description,req.body.Tags]);
+	console.log('process spawned');
+	process.stdout.on('data', data => {
+		console.log('sent');
+		res.send({percent: data.toString()});
+	})
+	process.stderr.on('data', data => {
+		console.log("ERROR: " + data);
+	})
+})
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port);
