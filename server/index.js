53d2df62ae5306575c4d5b5fb0d1dc091afe9a06
mickey henry parker <henryparker594@gmail.com>
2019-03-07 18:02:35 -0500

    fix Server

--- server/index.js
+++ server/index.js
@@ -27,7 +27,6 @@ app.post('/prediction', async(req,res) => {
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
