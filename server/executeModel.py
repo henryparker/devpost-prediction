5ea911747399d8eda8f1a0da67bd0a9e48090d2c
mickey henry parker <henryparker594@gmail.com>
2019-03-07 15:23:37 -0500

    update executeModel.py

--- server/executeModel.py
+++ server/executeModel.py
@@ -15,6 +15,7 @@ Tag = sys.argv[4]
 
 Total = Title +' '+ ShortPitch + ' ' + Description + ' ' + Tag
 
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from sklearn.externals import joblib
 import pickle
@@ -23,7 +24,7 @@ stop_words = text.ENGLISH_STOP_WORDS.union(["nan"])
 tv = TfidfVectorizer(decode_error="replace",ngram_range=(1,2),stop_words=stop_words,vocabulary=pickle.load(open("feature.pkl", "rb")))
 res_feature = tv.fit_transform([Total])
 logreg = joblib.load('final_1Model.sav')
-res = logreg.predict_proba(tmp)
+res = logreg.predict_proba(res_feature)
 print(res[1])
 
 
