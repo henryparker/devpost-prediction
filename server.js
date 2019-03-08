58480922a0bf71d3c956ca22c0344278536bdfb7
Jay-Mithani <jayvmithani@gmail.com>
2019-03-07 19:32:25 -0500

    port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-let port = 5000 || process.env.PORT;
+let port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client", "build")))
 
