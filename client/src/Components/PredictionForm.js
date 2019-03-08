e16d0954b246426df2b45359bea8edbe10901aac
Jay-Mithani <jayvmithani@gmail.com>
2019-03-07 19:04:01 -0500

    client

---
+++ client/src/Components/PredictionForm.js
@@ -0,0 +1,106 @@
+/* eslint-disable no-unused-expressions */
+
+import React, {Component} from 'react';
+import {Input, Button, Header,Form,Checkbox, Container} from 'semantic-ui-react';
+import SubmitModal from './SubmitModal';
+import {Formik} from 'formik';
+import axios from 'axios';
+import 'semantic-ui-css/semantic.min.css';
+
+export default class PredictionForm extends Component{
+
+    constructor(props){
+      super(props);
+      this.state = {
+		winProbability: null,
+		openModal: false
+      }
+	}
+    render(){
+
+
+        return (
+        <div style={{marginTop:'10%'}}>
+          	<Container style={{backgroundColor:"white", borderRadius:"25px",boxShadow: "2px 2px 5px #fc6767", padding:"10px"}}>
+				<Header as="h1">Hackathon Predictor</Header>
+				<Formik
+					initialValues={{
+						Title: "", 
+						ShortPitch: "",
+						Description: "", 
+						Tags: "",
+					}}
+					onSubmit={(values) => {
+						let submission = {
+							...values,
+							Tags: values.Tags.split(",").map(tag => tag.toLowerCase().trim())
+						};
+						console.log('submitted');
+						console.log(submission);
+						axios.post('/prediction', submission)
+						.then((res) => {
+							console.log('received');
+							this.setState((state, props) => {
+								return {openModal: true, winProbability: res.data.percent};
+							});
+						})
+						.catch((err) => {
+							console.log(err);
+						})
+					}}
+					render={props => (
+						<div style={{ padding:"10px"}}>
+							<Form onSubmit={props.handleSubmit}>
+								<Form.Group widths='equal'>
+									<Form.Input
+										fluid label='Title'
+										placeholder='Title' 
+										onChange={props.handleChange}
+										name="Title"
+										values={props.values.Title}
+									/>
+								</Form.Group>
+								<Form.Group widths='equal'>
+									<Form.Input
+										fluid label='Short Pitch' 
+										placeholder='Short pitch' 
+										onChange={props.handleChange}
+										name="ShortPitch"
+										values={props.values.ShortPitch}
+									/>
+								</Form.Group>
+								<Form.TextArea
+									label='Description'
+									placeholder='Tell us more about your project...' 
+									onChange={props.handleChange}
+									name="Description"
+									values={props.values.Description}
+								/>
+								<Form.Group widths='equal'>
+									<Form.Input
+										fluid label='Tags' 
+										placeholder='NodeJS, React, Express...' 
+										onChange={props.handleChange}
+										name="Tags"
+										values={props.values.Tags}
+									/>
+								</Form.Group>
+								<Form.Button color="blue" type="submit" >Submit</Form.Button>
+							</Form>
+						</div>
+					)}
+				/>
+          		<SubmitModal
+					open={this.state.openModal}
+					winProbability={this.state.winProbability}
+					closer={() => this.setState((state, props) => {
+						return {openModal: false}
+					})}
+				>
+				</SubmitModal>
+        	</Container>
+        </div>
+        );
+    }
+  
+}
---
+++ client/src/Components/SubmitModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Modal, Button} from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+const BASE_ADV = 0.07;
+
+const advantage = (percent) => {
+	let advStr = "";
+	advStr += percent / BASE_ADV;
+	advStr += "x";
+	return advStr;
+}
+
+class SubmitModal extends React.PureComponent {
+	render() {
+		return (
+			<Modal open={this.props.open}>
+				<Modal.Content>
+					<Modal.Description>
+						<p>Your advantage is: {advantage(this.props.winProbability)}</p>
+					</Modal.Description>
+					<Button color='green' style={{marginLeft: '90%'}}onClick={this.props.closer}>Done</Button>
+				</Modal.Content>
+			</Modal>
+		);
+	}
+}
+
+export default SubmitModal;
